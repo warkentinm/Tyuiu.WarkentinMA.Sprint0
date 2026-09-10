8d0195c84021d59f6184179aae84c87f3af5461d
Tyuiu.WarkentinMA@outlook.com <Tyuiu.WarkentinMA@outlook.com>
2026-09-10 16:03:45 +0500

    Task3

---
+++ Tyuiu.WarkentinMA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.WarkentinMA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.WarkentinMA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+             
+        }
+    }
+}
