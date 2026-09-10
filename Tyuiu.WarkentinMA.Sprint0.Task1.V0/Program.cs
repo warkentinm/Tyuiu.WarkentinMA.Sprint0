69b77394f763e37bc868de79a23b7481cc79b893
Tyuiu.WarkentinMA@outlook.com <Tyuiu.WarkentinMA@outlook.com>
2026-09-10 14:43:45 +0500

    Task1

---
+++ Tyuiu.WarkentinMA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.WarkentinMA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Максим";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет! ");
+            Console.ReadKey();
+        }
+    }
+}
