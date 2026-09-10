b68b45090d2665860a775fe33479c086ee8cb5dd
Tyuiu.WarkentinMA@outlook.com <Tyuiu.WarkentinMA@outlook.com>
2026-09-10 15:26:04 +0500

    Task2

---
+++ Tyuiu.WarkentinMA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.WarkentinMA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.WarkentinMA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Максим";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Максим", res);
+        }
+    }
+}
---
+++ Tyuiu.WarkentinMA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.WarkentinMA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.WarkentinMA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Максим"));
+            Console.ReadKey();
+        }
+    }
+}
