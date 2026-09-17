999e00ff39f3667f15595a45c196bc12399e37af
Tyuiu.WarkentinMA@outlook.com <Tyuiu.WarkentinMA@outlook.com>
2026-09-17 21:01:58 +0500

    Task4

---
+++ Tyuiu.WarkentinMA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.WarkentinMA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.WarkentinMA.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
