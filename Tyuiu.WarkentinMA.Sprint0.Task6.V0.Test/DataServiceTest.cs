da4e6c0e88fef348fdb52c4e3f9d7802116ee0dc
Tyuiu.WarkentinMA@outlook.com <Tyuiu.WarkentinMA@outlook.com>
2026-09-22 23:41:45 +0500

    Task6

---
+++ Tyuiu.WarkentinMA.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.WarkentinMA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.WarkentinMA.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckSubstractioArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        [TestMethod]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+
+    }
+}
